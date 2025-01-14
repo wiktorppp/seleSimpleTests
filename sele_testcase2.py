c41dbbb097157226ffc612d0ae666765bf19275d
wiktorppp <wiktor135799@gmail.com>
2025-01-14 22:43:34 +0100

    Changes in sele_testcase2.py

--- sele_testcase2.py
+++ sele_testcase2.py
@@ -3,41 +3,42 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Initialize the WebDriver (ensure the correct path for your driver)
-driver = webdriver.Edge()
-#driver1 = webdriver.Firefox()
-#driver2 = webdriver.Chrome()
-
-try:
-    # Open the website
-    driver.get("https://onet.pl/")
-
-    # Verify the page title
-    expected_title = "Onet"
-    assert expected_title in driver.title, f"Expected title '{expected_title}' not found in '{driver.title}'"
-    print("Page title verification passed.")
-
-    # Wait for the page to load
-    time.sleep(3)
-
+def main_test_def(driver):
     try:
-        accept_cookies = driver.find_element(By.XPATH,"/html/body/div[10]/div/div[2]/div/div[6]/button[2]")
-        accept_cookies.click()
-        print("Cookies accepted!")
+        # Open the website
+        driver.get("https://tester-wp.azurewebsites.net/")
 
+        # Verify the page title
+        expected_title = "Home Page - MyMvcApp"
+        assert expected_title in driver.title, f"Expected title '{expected_title}' not found in '{driver.title}'"
+        print("Page title verification passed.")
+
+        # Wait for the page to load
         time.sleep(3)
-    except:
-        print("Accepting failed:", e)
-    
-    try:
-        selenium_training_link = driver.find_element(By.CLASS_NAME, "Logo_logoLink__qUFvz")
-        selenium_training_link.click()
-        print("Successfully clicked on 'Logo header'.")
-        
-        time.sleep(3)  # Let the new page load
-    except Exception as e:
-        print("Logo interaction failed:", e)
 
-finally:
-    # Close the browser
-    driver.quit()
+        try:
+            home_page_link = driver.find_element(By.XPATH,"/html/body/header/nav/div/div/ul/li[1]/a")
+            home_page_link.click()
+            print("Successfully clicked on 'Home'!")
+
+            time.sleep(3)
+        except:
+            print("'Home' interaction failed:", e)
+
+        try:
+            privacy_link = driver.find_element(By.LINK_TEXT, "Privacy")
+            privacy_link.click()
+            print("Successfully clicked on 'Privacy'!")
+                
+            time.sleep(3)  # Let the new page load
+        except Exception as e:
+            print("Privacy interaction failed:", e)
+
+    finally:
+        # Close the browser
+        driver.quit()
+
+# Initialize the WebDriver (ensure the correct path for your driver)
+main_test_def(webdriver.Edge())
+main_test_def(webdriver.Firefox())
+main_test_def(webdriver.Chrome())
